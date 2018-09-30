2c020dee9197f0f865568bd6eb4a64deeae4dbc2
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-09-30 11:44:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  kittens.
+}
